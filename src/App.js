b51b77cae8fc5fde148d159d60c1c7e4a55086a7
codeguruvaa <mohanvej@gmail.com>
2022-07-18 16:43:33 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,35 +1,141 @@
 import React from 'react';
-import Header from './Components/Header';
-import Slider from './Components/Slider';
-
-var pdesign ={
-  width:'950px',
-  backgroundColor:'gray',
-  padding:'10px'
-}
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
 
-      <Header />
-      <Slider />
+      <nav class="navbar navbar-expand-lg bg-info">
 
-      <h2 style={{color:'orange', backgroundColor:'cyan'}}>Heading</h2>
+        <div class="container">
 
-      <p style={pdesign}>
-        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-      </p>
+          <a class="navbar-brand" href="#">Navbar</a>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider" /></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+
+      </nav>  
 
 
-      
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+
+        <div class="carousel-inner">
+
+          <div class="carousel-item active">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+
+
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+
+      </div>
+
+
+
+    <div className='container mt-4'>
+
+        <div className='row'>
+
+          <div className='col-sm-6 col-lg-9'>
+            <p className='bg-info p-5'>
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+            </p>
+          </div>
+
+          <div className='col-sm-6 col-lg-3'>
+            <p className='bg-danger p-5'>
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog the quikc brown fox jump over the lazy dog
+              the quikc brown fox jump over the lazy dog 
+            </p>
+          </div>
+
+          </div>
+      </div>
+
+
     </div>
   );
 }
